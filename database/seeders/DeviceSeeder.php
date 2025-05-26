aa48897beb31555b3aa07bd12b9f8b7200f8edb1
wolfis21 <isaacsaado69@gmail.com>
2025-05-26 16:13:35 -0400

    update seeder

--- database/seeders/DeviceSeeder.php
+++ database/seeders/DeviceSeeder.php
@@ -17,7 +17,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Smart Breaker de aire acondicionado cuarto principal',
             'type' => 'climatización',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/airconditioner',
             'location' => 'aire principal',
             'users_id' => 1,
         ]);
@@ -25,7 +25,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Smart Breaker de aire acondicionado cuarto secundario',
             'type' => 'climatización',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/airconditioner2',
             'location' => 'aire de visita',
             'users_id' => 1,
         ]);
@@ -34,7 +34,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Smart Breaker de nevera de la cocina',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/fridge',
             'location' => 'Cocina',
             'users_id' => 1,
         ]);
@@ -43,7 +43,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Smart Breaker de lavadora-Secadora',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/washer_dryer',
             'location' => 'Cocina',
             'users_id' => 1,
         ]);
@@ -52,7 +52,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Bombillo sala',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/energy1',
             'location' => 'Sala',
             'users_id' => 1,
         ]);
@@ -61,7 +61,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Bombillo cuarto 1',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/energy2',
             'location' => 'cuarto principal',
             'users_id' => 1,
         ]);
@@ -70,7 +70,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Bombillo cuarto 2',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/energy3',
             'location' => 'cuarto de visita',
             'users_id' => 1,
         ]);
@@ -79,7 +79,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Bombillo baño',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/energy5',
             'location' => 'baño',
             'users_id' => 1,
         ]);
@@ -88,7 +88,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Bombillo cocina',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/energy4',
             'location' => 'Cocina',
             'users_id' => 1,
         ]);
@@ -97,7 +97,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Smart Breaker de cocina electrica',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/kitchen',
             'location' => 'Cocina',
             'users_id' => 1,
         ]);
@@ -106,7 +106,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Termostato',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/temperature/humedad',
             'location' => 'General',
             'users_id' => 1,
         ]);
@@ -115,7 +115,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Sensor de humo',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/sensor',
             'location' => 'General',
             'users_id' => 1,
         ]);
@@ -124,7 +124,7 @@ class DeviceSeeder extends Seeder
             'name' => 'Smart Breaker del calentador de agua',
             'type' => 'electrodoméstico',
             'status' => 'activo',
-            'protocol' => 'MQTT',
+            'protocol' => 'iot/water_heater',
             'location' => 'General',
             'users_id' => 1,
         ]);
